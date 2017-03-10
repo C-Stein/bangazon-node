d0b78c21f8f4acd8a421cb99b08df155ce28617b
Caitlin Stein <caitlin.teter@gmail.com>
2017-03-10 15:50:25 -0600

    added module for creating customers

--- createCustomer.js
+++ createCustomer.js
@@ -1,5 +1,9 @@
 'use strict'
 
+const { Database } = require('sqlite3').verbose();
+
+const db = new Database('db/bangazon.sqlite');
+
 const readlineSync = require('readline-sync');
 
 let createCustomer = () => {
@@ -9,6 +13,22 @@ let createCustomer = () => {
   let state = readlineSync.question("What is your state? ")
   let zipcode = readlineSync.question("What is your zipcode? ")
   let phoneNumber = readlineSync.question("What is your phone number? ")
+
+  let query = `INSERT INTO customers VALUES (null, "${name}", "${address}", "${city}", "${state}", "${zipcode}", "${phoneNumber}")`
+  console.log("query", query)
+  db.run(query, (err) => {
+    console.log("ERR:", err)
+  })
+
+  require("./closeDB")
 }
 
-module.exports = { createCustomer }
+let listCustomers = () => {
+  console.log("finding customers")
+  db.all(`SELECT name FROM customers`, (err, rows) => {
+    console.log(rows)
+  })
+  require("./closeDB")
+}
+
+module.exports = { createCustomer, listCustomers }
